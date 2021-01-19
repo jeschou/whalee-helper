36b81b39964eede645f0dd792854804f1eb32f08
Jessen <shenyangzhou@whale.im>
2021-01-19 20:49:07 +0800

    use jetcd

--- src/main/java/cn/whale/helper/utils/EtcdUtil.java
+++ src/main/java/cn/whale/helper/utils/EtcdUtil.java
@@ -1,94 +1,50 @@
 package cn.whale.helper.utils;
 
-import com.google.gson.Gson;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.etcd.jetcd.ByteSequence;
+import io.etcd.jetcd.Client;
+import io.etcd.jetcd.KeyValue;
+import io.etcd.jetcd.kv.GetResponse;
+import io.etcd.jetcd.options.GetOption;
+import io.etcd.jetcd.resolver.URIResolverLoader;
 
-import java.io.OutputStream;
-import java.net.URL;
-import java.net.URLConnection;
-import java.net.URLEncoder;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 
 public class EtcdUtil {
-    static final String etcdkeeper_host = "http://etcdkeeper.meetwhale.com:12000/v3";
-    static final String etcd_host = "etcd-pro.meetwhale.com:5379";
+    static final String etcd_endpoint = "http://etcd-pro.meetwhale.com:5379";
     static final String path = "/develop/config/pgsql";
+    static Charset UTF_8 = StandardCharsets.UTF_8;
 
-    static Gson gson = new Gson();
+    static ObjectMapper mapper = new ObjectMapper();
 
-
-    static long sessionAliveUntil = 0;
-    static String sessionId = "";
-
-    public static String getEtcdKeeperSessionId() throws Exception {
-        if (Utils.isNotEmpty(sessionId) && sessionAliveUntil > System.currentTimeMillis()) {
-            return sessionId;
-        }
-        sessionId = null;
-        URL url = new URL(etcdkeeper_host + "/connect");
-        URLConnection conn = url.openConnection();
-        conn.setDoInput(true);
-        conn.setDoOutput(true);
-
-        conn.connect();
-        OutputStream os = conn.getOutputStream();
-        os.write(("host=" + etcd_host).getBytes());
-        Utils.safeClose(os);
-        Utils.readText(conn.getInputStream());
-        Map<String, List<String>> headerMap = conn.getHeaderFields();
-        List<String> cookies = headerMap.get("Set-Cookie");
-        if (cookies == null || cookies.size() == 0) {
-            return null;
-        }
-        String[] segs = cookies.get(0).split(";");
-        for (String str : segs) {
-            str = str.trim();
-            String[] strs = str.split("=");
-            if (strs.length != 2) {
-                continue;
-            }
-            if ("_etcdkeeper_session".equals(strs[0])) {
-                sessionId = strs[1];
-            } else if ("Max-Age".equals(strs[0])) {
-                sessionAliveUntil = System.currentTimeMillis() + Integer.parseInt(strs[1]) * 1000;
-            }
-        }
-
-        return sessionId;
+    static ByteSequence BS(String s) {
+        return ByteSequence.from(s, UTF_8);
     }
-
     static Set<String> skipCfg = new HashSet<>(Arrays.asList("db_host", "clickhouse", "hive"));
 
     public static List<DbConfig> getDbConfigList() throws Exception {
+        new io.etcd.jetcd.resolver.SmartNameResolver(null, null, URIResolverLoader.defaultLoader());
         List<DbConfig> list = new ArrayList<>();
-        URL url = new URL(etcdkeeper_host + "/getpath?prefix=true&key=" + URLEncoder.encode(path, "UTF-8"));
-        URLConnection conn = url.openConnection();
-        conn.addRequestProperty("Cookie", "etcd-endpoint=" + etcd_host + "; _etcdkeeper_session=" + getEtcdKeeperSessionId());
-        conn.connect();
-        String resp = Utils.readText(conn.getInputStream());
-        VirtualNode virtualNode = gson.fromJson(resp, VirtualNode.class);
-        for (KV kv : virtualNode.node.nodes) {
-            String serviceName = Utils.substringAfterLast(kv.key, "/");
-            if (skipCfg.contains(serviceName)) {
-                continue;
+        try (Client etcdClient = Client.builder().endpoints(etcd_endpoint).build()) {
+            GetResponse getResponse = etcdClient.getKVClient().get(BS(path), GetOption.newBuilder().withPrefix(BS(path)).build()).get();
+            for (KeyValue kv : getResponse.getKvs()) {
+                if (kv.getValue().isEmpty()) continue;
+
+                try {
+                    DbConfig cfg = mapper.readValue(kv.getValue().getBytes(), DbConfig.class);
+                    cfg.serviceName = Utils.substringAfterLast(kv.getKey().toString(UTF_8), "/");
+                    if (skipCfg.contains(cfg.serviceName)) {
+                        continue;
+                    }
+                    list.add(cfg);
+                } catch (Exception e) {
+                    //e.printStackTrace();
+                }
             }
-            DbConfig dbConfig = gson.fromJson(kv.value, DbConfig.class);
-            dbConfig.serviceName = serviceName;
-            list.add(dbConfig);
         }
         return list;
-    }
-
-    public static class VirtualNode {
-        public Node node;
-    }
-
-    public static class Node {
-        public KV[] nodes;
-    }
-
-    public static class KV {
-        public String key;
-        public String value;
     }
 
 }
--- src/main/java/icons/PluginIcons.java
+++ src/main/java/icons/PluginIcons.java
@@ -7,4 +7,5 @@ import javax.swing.*;
 public interface PluginIcons {
     Icon BAZEL = IconLoader.getIcon("/icons/bazel-icon.svg");
     Icon PROTOBUF = IconLoader.getIcon("/icons/protobuf.svg");
+    Icon REPO = IconLoader.getIcon("/icons/repo.svg");
 }
